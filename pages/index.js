a36ebcca49dcf5bb4ea90183f03ab2be911db975
Albarral19 <jalbarral96@gmail.com>
2023-09-17 23:08:40 -0300

    msg default

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Eu gosto muito da sua companhia. É muito bom sentir o carinho que você tem por mim! sz <br></br>Obrigado por tudo!</h1>
+    return <h1>Domenico Berardi!</h1>
 }
 
 export default Home;
